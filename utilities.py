8a5cf408b9651234504f7eda79968f4aec377e07
lucs100 <lucs100d@gmail.com>
2025-10-26 16:36:42 -0400

    Fix yaw calculation and add logging + debug code

--- utilities.py
+++ utilities.py
@@ -1,4 +1,4 @@
-from math import atan2, asin, sqrt
+from math import atan2, acos, sqrt
 from geometry_msgs.msg import Quaternion
 
 M_PI=3.1415926535
@@ -67,14 +67,43 @@ def euler_from_quaternion(quat):
     assert isinstance(quat, Quaternion), f"You must pass a Quaternion. Got {type(quat)}."
     #x, y, z, w = quat
 
-    # assume phi = [ 0 0 1 ]', so x = 0, y = 0, z = cos(yaw/2), w = sin(yaw/2)  
-    # yaw = asin(quat.w)*2
-    a = 2 * (quat.w * quat.z + quat.x + quat.y)
-    b = 1 - (2 * (quat.y ** 2 + quat.z ** 2))
-    yaw = atan2(a, b)
+    # assume phi = [ 0 0 1 ]', so x = 0, y = 0, z = sin(yaw/2), w = cos(yaw/2)  
+    # yaw = acos(quat.w)*2
+    yaw = acos(quat.w)*2
+    # experimentally, it looks like the phi direction swaps every full revolution?
+    # we need to check the sign of z - if z is negative, then "invert" the yaw
+    if quat.z < 0:
+        yaw = 2*M_PI - yaw
 
-    ... # just unpack yaw
+    # print(f"Yaw: {yaw:.2f} rad / {(yaw/M_PI):.2f}pi rad / {(yaw*180/M_PI):.2f} deg \t [{quat.x:.2f}, {quat.y:.2f}, {quat.z:.2f}, {quat.w:.2f}]")
     return yaw
+
+    # experimental code archived below for posterity; not useful here but shown as proof
+
+    # odom_orientation = {'x': msg.pose.pose.orientation.x,
+    #                     'y': msg.pose.pose.orientation.y, 
+    #                     'z': msg.pose.pose.orientation.z, 
+    #                     'w': msg.pose.pose.orientation.w}
+    # q = ['dummy', odom_orientation['x'], odom_orientation['y'], odom_orientation['z'], odom_orientation['w']]
+    # Ra = [+q[1]**2-q[2]**2-q[3]**2+q[4]**2,   2*(q[1]*q[2] - q[4]*q[3]),    2*(q[2]*q[4] + q[1]*q[3])]
+    # Rb = [2*(q[4]*q[3] - q[1]*q[2]),   -q[1]**2+q[2]**2-q[3]**2+q[4]**2,    2*(q[2]*q[3] - q[1]*q[4])]
+    # Rc = [2*(q[1]*q[3] - q[4]*q[2]),   2*(q[4]*q[1] + q[2]*q[3]),    -q[1]**2-q[2]**2+q[3]**2+q[4]**2]
+
+    # print("\nRotation matrix:")
+    # print("\n".join([', '.join([f'{x:.2f}' for x in r]) for r in [Ra, Rb, Rc]]))
+
+    # angle = acos(q[1]**2-q[2]**2-q[3]**2+q[4]**2)
+    # print(f"\t-> Rotation angle: {angle:.2f} [{(angle/pi):.2f}pi]\n")
+
+    # odom_x_pos = msg.pose.pose.position.x
+    # odom_y_pos = msg.pose.pose.position.y
+    # angle_z = 2*asin(odom_orientation['z'])
+    # angle_w = 2*acos(odom_orientation['w'])
+
+    # if odom_orientation['z'] < 0:
+    #     true_angle = 2*pi - angle_w
+    # else:
+    #     true_angle = angle_w
 
 
 #DONE Part 4: Implement the calculation of the linear error
@@ -113,10 +142,10 @@ def calculate_angular_error(current_pose, goal_pose):
     error_angular = min(M_PI, error_angular)
     error_angular = max(-M_PI, error_angular)
     
-    print(f"""Current coords: {(round(current_pose[0], 2), round(current_pose[1], 2))} 
-          Goal vector: {(round(goal_pose[1]-current_pose[1], 2), round(goal_pose[1]-current_pose[1], 2))} 
-          Best heading: {round(bestHeading, 2)} 
-          Current heading: {round(currentHeading, 2)} 
-          Error: {error_angular}""")
+    print(f"XY: ({(current_pose[0]):.2f}, {(current_pose[1]):.2f}) -> ({(goal_pose[0]):.2f}, {(goal_pose[1]):.2f}): "
+          f"({(goal_pose[0]-current_pose[0]):.2f}, {(goal_pose[1]-current_pose[1]):.2f})\t"
+          f"Th_b: {bestHeading:.2f} "
+          f"Th_c: {currentHeading:.2f} "
+          f"E_th: {error_angular:.2f} ")
     
     return error_angular
